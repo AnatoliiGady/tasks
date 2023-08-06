23ff1a67ae79bf7c22f400682e4ea4e53a112125
AnatoliiGady <grek-prost@yandex.ru>
2023-08-06 21:26:27 +0900

    arrays: Task20

--- src/main/java/org/example/arrays/Task20.java
+++ src/main/java/org/example/arrays/Task20.java
@@ -7,18 +7,19 @@ public class Task20 {
     public static void main(String[] args) {
         int[] array = {2, 4, -1, 2};
         System.out.println(Arrays.toString(deleteElementArray(array, 1)));
-
     }
-// изменить на массив другой
+
     static int[] deleteElementArray(int[] numbers, int index) {
         int[] array = new int[numbers.length - 1];
-        int j = 0;
         for (int i = 0; i < numbers.length; i++) {
-            if (i != index) {
-                array[j] = numbers[i];
-                j++;
+            if (i > index) {
+                array[i - 1] = numbers[i];
+            }
+            if (i < index) {
+                array[i] = numbers[i];
             }
         }
+
         return array;
     }
 }
